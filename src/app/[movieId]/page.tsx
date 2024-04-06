09ac2e0bc12d9e0006a2f385eab6979472ea2f70
buistvo <plus100500@gmail.com>
2024-04-07 00:29:58 +0300

    add total movies

--- src/app/[movieId]/page.tsx
+++ src/app/[movieId]/page.tsx
@@ -39,16 +39,17 @@ export default async function Page({
         </div>
         <MovieDetails movie={movie} />
       </div>
-      <div>
+      <div className="bg-workspace">
         <GenreSelect
           initialSelectedGenre={searchParams?.genre || ''}
           genreList={GENRE_LIST_OPTIONS}
         ></GenreSelect>
-      </div>
-      <div className="bg-workspace flex flex-wrap mt-2">
-        {movies.data.map((movie) => (
-          <MovieTile key={movie.id} movie={movie}></MovieTile>
-        ))}
+        {movies.totalAmount} MOVIES FOUND
+        <div className="bg-workspace flex flex-wrap mt-2">
+          {movies.data.map((movie) => (
+            <MovieTile key={movie.id} movie={movie}></MovieTile>
+          ))}
+        </div>
       </div>
     </div>
   );
